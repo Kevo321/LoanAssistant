6af1184e1b10821531f238a6a24f6925bc814b86
Kevo321 <89575370+Kevo321@users.noreply.github.com>
2022-07-19 21:41:50 +0100

    Add files via upload

---
+++ loanAssistant/LoanAssistantTester.java
@@ -0,0 +1,12 @@
+package loanAssistant;
+
+public class LoanAssistantTester {
+
+	public static void main(String[] args) {
+		 // create new frame
+        new LoanAssistant().setVisible(true);
+   
+
+	}
+
+}
